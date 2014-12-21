74f874d0f0af0f51e242224e015045a216b0756b
Tejas Manohar <me@tejasmanohar.com>
2014-12-21 11:41:49 -0600

    return request

--- app.js
+++ app.js
@@ -5,38 +5,51 @@ var schedule = require('node-schedule');
 var twilio = require('twilio');
 
 
-// // Setup Rule
-// var rule = new schedule.RecurrenceRule();
-// rule.dayOfWeek = [0, new schedule.Range(0, 6)];
-// rule.hour = 14;
-// rule.minute = 0;
+// Setup Rule
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+rule.hour = 14;
+rule.minute = 0;
+
+var j = schedule.scheduleJob(date, function(){
+  db.list('subscribers')
+    .then(function (result) {
+      var data = result.body.results;
+      data.forEach(function(obj) { sendMMS(obj.path.key); })
+    })
+    .fail(function (err) {
+      console.log(err)
+    })
+});
 
 
 // Get Top Post of Day from Imgur
-function getTopPostUrl() {
-  request
+function getTopPost() {
+  return request
     .get('https://api.imgur.com/3/gallery/top/top/day/1?showViral=false')
     .set('Authorization', 'Client-ID ' + process.env.IMGUR_CLIENT_ID)
     .set('Accept', 'application/json')
-    .end(function(err, res) {
-      if(err) {
-        console.log(err);
-      } else {
-        json = JSON.parse(res.text);
-        console.log(json.data[0].link);
-      }
-    });
 }
-getTopPostUrl();
+
+getTopPost().end(function(err, res) {
+    item = JSON.parse(res.text).data[0];
+    return [item.title, item.link];
+});
+
+
 // Send MMS to Subscriber
-
-
-// // Loop through Orchestrate Collection
-// db.list('subscribers')
-//   .then(function (result) {
-//     var data = result.body.results;
-//     data.forEach(function(obj) { console.log(obj.path.key); })
-//   })
-//   .fail(function (err) {
-//     console.log(err)
-//   })
+function sendMMS(recipient) {
+  var post = getTopPost();
+  client.sendMessage({
+    to: recipient,
+    body: post[0],
+    mediaUrl: post[1],
+    from: process.env.TWILIO_NUMBER
+  }, function(err, messageData) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Message sent to ' + recipient);
+      }
+  });
+}
