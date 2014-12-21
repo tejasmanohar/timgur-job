f5ca53d9f65b0d6f85b6ea885dac18bfc5da5a9e
Tejas Manohar <me@tejasmanohar.com>
2014-12-21 11:09:51 -0600

    grab from imgur

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 // Require Modules
-var db = require('orchestrate')(process.env.API_KEY)
+// var db = require('orchestrate')(process.env.API_KEY)
 var request = require('superagent');
 var schedule = require('node-schedule');
+var twilio = require('twilio');
 
 
-// Setup Rule
-var rule = new schedule.RecurrenceRule();
-rule.dayOfWeek = [0, new schedule.Range(0, 6)];
-rule.hour = 14;
-rule.minute = 0;
+// // Setup Rule
+// var rule = new schedule.RecurrenceRule();
+// rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+// rule.hour = 14;
+// rule.minute = 0;
 
 
 // Get Top Post of Day from Imgur
-
-
+function getTopPostUrl() {
+  request
+    .get('https://api.imgur.com/3/gallery/top/top/day/1?showViral=false')
+    .set('Authorization', 'Client-ID ' + process.env.IMGUR_CLIENT_ID)
+    .set('Accept', 'application/json')
+    .end(function(err, res) {
+      if(err) {
+        console.log(err);
+      } else {
+        json = JSON.parse(res.text);
+        console.log(json.data[0].link);
+      }
+    });
+}
+getTopPostUrl();
 // Send MMS to Subscriber
 
 
-// Loop through Orchestrate Collection
-db.list('subscribers')
-  .then(function (result) {
-    var data = result.body.results;
-    data.forEach(function(obj) { console.log(obj.path.key); })
-  })
-  .fail(function (err) {
-    console.log(err)
-  })
+// // Loop through Orchestrate Collection
+// db.list('subscribers')
+//   .then(function (result) {
+//     var data = result.body.results;
+//     data.forEach(function(obj) { console.log(obj.path.key); })
+//   })
+//   .fail(function (err) {
+//     console.log(err)
+//   })
