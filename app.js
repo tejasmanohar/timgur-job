9c29d9ce43c7a5678a73e57a5cb94c75d72fb06e
Tejas Manohar <me@tejasmanohar.com>
2014-12-21 06:46:08 -0600

    loop thru orchestrate collection

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0, new schedule.Range(0, 6)];
 rule.hour = 14;
 rule.minute = 0;
+
+// Loop Through Orchestrate Collection
+db.list('subscribers')
+  .then(function (result) {
+    var data = result.body.results;
+    console.log(data);
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        console.log(data[key].key);
+      }
+    }
+  })
+  .fail(function (err) {
+    console.log(err)
+  })
