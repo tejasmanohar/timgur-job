9e981f2d4b067fc0d0183cc0acbbb5eed211ad6d
Tejas Manohar <me@tejasmanohar.com>
2014-12-21 06:18:56 -0600

    new rule

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 // Require Modules
 var db = require('orchestrate')(process.env.API_KEY)
 var schedule = require('node-schedule');
+
+// Setup Rule
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+rule.hour = 14;
+rule.minute = 0;
