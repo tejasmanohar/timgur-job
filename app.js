837d7eb2ec4243286c0038616f20dd1a8a12b3d1
Tejas Manohar <me@tejasmanohar.com>
2014-12-21 12:10:40 -0600

    unexpected

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Require Modules
-// var db = require('orchestrate')(process.env.API_KEY)
+var db = require('orchestrate')(process.env.ORCHESTRATE_API_KEY)
 var request = require('superagent');
 var schedule = require('node-schedule');
-var twilio = require('twilio');
+var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 
 // Setup Rule
@@ -11,6 +11,7 @@ rule.dayOfWeek = [0, new schedule.Range(0, 6)];
 rule.hour = 14;
 rule.minute = 0;
 
+// Schedule Task
 var j = schedule.scheduleJob(date, function(){
   db.list('subscribers')
     .then(function (result) {
@@ -24,32 +25,30 @@ var j = schedule.scheduleJob(date, function(){
 
 
 // Get Top Post of Day from Imgur
-function getTopPost() {
-  return request
+function getTopPost(cb) {
+  request
     .get('https://api.imgur.com/3/gallery/top/top/day/1?showViral=false')
     .set('Authorization', 'Client-ID ' + process.env.IMGUR_CLIENT_ID)
     .set('Accept', 'application/json')
+    .end(function(err, res) {
+      item = JSON.parse(res.text).data[0];
+      cb([item.title, item.link]);
+    })
 }
-
-getTopPost().end(function(err, res) {
-    item = JSON.parse(res.text).data[0];
-    return [item.title, item.link];
-});
 
 
 // Send MMS to Subscriber
 function sendMMS(recipient) {
-  var post = getTopPost();
-  client.sendMessage({
-    to: recipient,
-    body: post[0],
-    mediaUrl: post[1],
-    from: process.env.TWILIO_NUMBER
-  }, function(err, messageData) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('Message sent to ' + recipient);
-      }
-  });
+  getTopPost(function(post) {
+    client.sendMessage({
+      to: recipient,
+      body: post[0],
+      mediaUrl: post[1],
+      from: process.env.TWILIO_NUMBER
+    }, function(err, messageData) {
+        if (err) {
+          console.error(err);
+        }
+    });
+  }
 }
