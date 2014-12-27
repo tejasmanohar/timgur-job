f8e3f2a0fa22a182795025a648aad209ed2cd19a
Tejas Manohar <me@tejasmanohar.com>
2014-12-26 22:17:00 -0600

    yay

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
-// Log All Uncaught Exceptions
-process.on('uncaughtException', function(err) {
-  console.log(err);
-});
-
-
-// Require Modules
+// Require Modul
+var express = require('express');
+var app = express();
 var db = require('orchestrate')(process.env.ORCHESTRATE_API_KEY)
 var request = require('superagent');
 var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-
-// Iterate through Orchestrate Collection
-db.list('subscribers')
-  .then(function (result) {
-    var data = result.body.results;
-    data.forEach(function(obj) { sendMMS(obj.path.key); })
-  })
-  .fail(function (err) {
-    console.log(err)
-  })
+// Setup Express
+var port = process.env.PORT || 3000;
+app.listen(port);
 
 
 // Get Top Post of Day from Imgur
@@ -53,3 +42,35 @@ function sendMMS(recipient) {
     });
   })
 }
+
+
+// Routes
+app.get('/', function(req, res) {
+  console.log('lol')
+  res.sendStatus(200);
+});
+
+app.get('/daily', function(req, res) {
+  res.sendStatus(200);
+  // db.list('subscribers')
+  //   .then(function (result) {
+  //     var data = result.body.results;
+  //     data.forEach(function(obj) { sendMMS(obj.path.key); })
+  //   })
+  //   .fail(function (err) {
+  //     console.log(err)
+  //   })
+  client.sendMessage({
+    to: '+1 214 436 1433',
+    body: 'test',
+    mediaUrl: 'http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg',
+    from: process.env.TWILIO_NUMBER
+  }, function(err, messageData) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(messageData)
+        console.log('lelelelelele')
+      }
+  });
+});
